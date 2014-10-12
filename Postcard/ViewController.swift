4e07f877baea27b3b2514fed39ba9fb13a048591
Emre <emrearas07@gmail.com>
2014-10-12 15:51:22 +0100

    name label fucntionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterMessageTextField: UITextField!
     
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
     @IBOutlet weak var mailButton: UIButton!
     
     
@@ -36,10 +40,27 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        
+        ////nameLabel.backgroundColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+        
+        
+        
+        
+        
         mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
+        
+        
         
     }
 
